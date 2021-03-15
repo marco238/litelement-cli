377d79bf9ffd7d562b8e6c67517a6ac44231ec76
Marcos <marcos.monzon@alten.es>
2021-03-15 16:07:11 +0100

    fix: added picomatch as a dev dependency

--- generators/app/templates/single_element/src/_element.js
+++ generators/app/templates/single_element/src/_element.js
@@ -38,8 +38,8 @@ class DemoElement extends LitElement {
       <h2>${this.title} Starter Kit</h2>
       <div class="text-container">
         <p>This is a LitElement base component.</p>
-        <p>For documentation open README file at directory's root</p>
-        <p>Made with ❤️ by <a rel="noreferrer" target="_blank" href="https://github.com/marco238">Marcos</a></p>
+        <p>Documentation in README file at directory's root or <a rel="noreferrer" target="_blank"  href="https://github.com/marco238/LitElement-Starter-Kit#readme">Github Repo</a></p>
+        <p>Made with ❤️ by <a rel="noreferrer" target="_blank" href="https://github.com/marco238">Marcos Monzón</a></p>
       </div>
     `;
   }
